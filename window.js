4090f619ac4ce86b624fb2d50b21d08461110d61
Agota Hanasz <agota.hanasz.1990@icloud.com>
2023-05-13 11:14:58 +0200

    added window

---
+++ window.js
@@ -0,0 +1,11 @@
+window.console.log("Szia");
+
+// - adott element JS reprezentációja
+// - miylen esemény kapcsán akarsz reagálni (onclick)
+// -milyen kódot akarsz futatni
+
+document.all[5].onclick = function() {
+    document.all[5].style.backgroundColor = "red";
+    console.log('OK')
+};
+
